12bc919ea23354d94871dedd1e7b38f4b22a7872
trashpew <christianmattson47@gmail.com>
2022-11-20 20:44:43 -0600

    Largest prime factor

---
+++ 3.py
@@ -0,0 +1,10 @@
+'''
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+6857
+'''
+
+from sympy import factorint
+print(factorint(600851475143))
