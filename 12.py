dd6edfcf4fb47177b03a560bab4353b3ce89e7e2
trashpew <christianmattson47@gmail.com>
2022-11-22 14:02:31 -0600

    unfinished

--- 12.py
+++ 12.py
@@ -11,7 +11,7 @@ num = 0
 for i in range(50000000000):
     num = num + i
     iter = 0
-    for x in range(1, int((num ** (1/2))) + 1):
+    for x in range(1, int(num / 2) + 1:
         if num % x == 0:
             iter += 1
         else:
