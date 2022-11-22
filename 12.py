c9b2b364a20099d39f9b83a715bf829b5731be26
trashpew <christianmattson47@gmail.com>
2022-11-22 13:53:32 -0600

    Highly divisible triangular number (unfinished)

---
+++ 12.py
@@ -0,0 +1,22 @@
+'''
+What is the value of the first triangle number to have over five hundred divisors?
+
+
+'''
+
+import sys
+
+i = 0
+num = 0
+for i in range(50000000000):
+    num = num + i
+    iter = 0
+    for x in range(1, int((num ** (1/2))) + 1):
+        if num % x == 0:
+            iter += 1
+        else:
+            continue
+    if iter >= 500:
+        print(num)
+        sys.exit()
+print("Finished.")
